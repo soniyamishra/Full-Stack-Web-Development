4596c014a52eee315e83994596c3fb01ec609000
soniya <mishrasoniya1999@gmail.com>
2020-08-08 17:01:49 +0530

    delete files

--- intro-to-node/index.js
+++
@@ -1,6 +0,0 @@
-const superheroes= require("superheroes");
-var mySuperheroes=superheroes.random();
-//console.log(mySuperheroes);
-const supervillains = require('supervillains');
-var mysupervillains=supervillains.random();
-console.log(mysupervillains);
