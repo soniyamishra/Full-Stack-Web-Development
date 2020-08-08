086e4af8b1bc2272c201f1e5153db91ee37b7005
soniya <mishrasoniya1999@gmail.com>
2020-08-08 17:02:25 +0530

    update

---
+++ intro-to-node/index.js
@@ -0,0 +1,6 @@
+const superheroes= require("superheroes");
+var mySuperheroes=superheroes.random();
+//console.log(mySuperheroes);
+const supervillains = require('supervillains');
+var mysupervillains=supervillains.random();
+console.log(mysupervillains);
